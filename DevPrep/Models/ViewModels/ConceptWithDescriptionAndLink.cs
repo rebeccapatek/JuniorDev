11220d9b0804edcfc636e7399b301d9f2edfd502
rebeccapatek <rebeccapatek@gmail.com>
2020-05-05 11:50:44 -0500

    added view models and started the concepts controller

---
+++ DevPrep/Models/ViewModels/ConceptWithDescriptionAndLink.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevPrep.Models.ViewModels
+{
+    public class ConceptWithDescriptionAndLink
+    {
+        public int ConceptId { get; set; }
+        public string ConceptName { get; set; }
+
+        public List<UsefulLink> UsefulLinks { get; set; }
+
+        public List<Description> Descriptions { get; set; }
+    }
+}
