dbff96934656a985443d5b8fd5191efbed3f7e15
rebeccapatek <rebeccapatek@gmail.com>
2020-05-06 14:29:51 -0500

    got new concept to add

--- DevPrep/Controllers/ConceptsController.cs
+++ DevPrep/Controllers/ConceptsController.cs
@@ -5,16 +5,19 @@ using System.Threading.Tasks;
 using DevPrep.Data;
 using DevPrep.Models;
 using DevPrep.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace DevPrep.Controllers
 {
+    [Authorize]
     public class ConceptsController : Controller
     {
-            
+
         private readonly ApplicationDbContext _context;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
@@ -27,17 +30,6 @@ namespace DevPrep.Controllers
         }
         // GET: Concept
         public async Task<ActionResult> Index(int? id)
-
-        //{
-        //    var concepts = from c in _context.Concepts
-        //                  .Include(c => c.SoftwareLanguage)
-        //                   select c;
-        //    if (id != null)
-        //    {
-        //        concepts = concepts.Where(c => c.SoftwareLanguageId == id);
-        //    }
-        //    return View(await concepts.ToListAsync());
-        //}
         {
             var model = new ConceptViewModel();
 
@@ -64,21 +56,58 @@ namespace DevPrep.Controllers
         }
 
         // GET: Concept/Create
-        public ActionResult Create()
+        [Route("Concepts/Create/{id}")]
+        public async Task<ActionResult> Create([FromRoute] int? id)
         {
-            return View();
+            
+
+            var technology = await _context.SoftwareLanguages.FirstOrDefaultAsync(SL => SL.id == id);
+        
+
+            var viewModel = new AddNewConceptwithDescriptionViewModel();
+
+            viewModel.SoftwareLanguage = technology;
+          
+
+            return View(viewModel);
         }
 
         // POST: Concept/Create
+        [Route("Concepts/Create/{id}")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult> Create(AddNewConceptwithDescriptionViewModel addNewConceptwithDescriptionViewModel, int id)
         {
             try
             {
+                var user = await GetCurrentUserAsync();
+                var concept= new Concept
+                {
+                    Name = addNewConceptwithDescriptionViewModel.ConceptName,
+                    SoftwareLanguageId = id,
+                    ApplicationUserId=user.Id
+                };
+                _context.Concepts.Add(concept);
+                await _context.SaveChangesAsync();
+                //Do I need to add the decription to a list of Descriptions in the Concept?
+                var description = new Description
+                {
+                    Paragraph = addNewConceptwithDescriptionViewModel.ConceptDescription,
+                    ApplicationUserId = user.Id,
+                    ConceptId = concept.id
+                };
+                _context.Descriptions.Add(description);
+                await _context.SaveChangesAsync();
+                
+
+                ///How do I grab the ConceptId from the concept I just created
+
+
+
+
                 // TODO: Add insert logic here
 
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Index), new {id = id });
             }
             catch
             {
@@ -131,5 +160,6 @@ namespace DevPrep.Controllers
                 return View();
             }
         }
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
     }
-}
+ }
