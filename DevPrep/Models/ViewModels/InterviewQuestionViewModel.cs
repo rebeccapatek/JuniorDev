ca9ec1524f4b30f54878a4f17777c3fdfe0849e8
rebeccapatek <rebeccapatek@gmail.com>
2020-05-25 16:05:21 -0500

    interview question index is working

---
+++ DevPrep/Models/ViewModels/InterviewQuestionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevPrep.Models.ViewModels
+{
+    public class InterviewQuestionViewModel
+    {
+        public List<InterviewQuestion> InterviewQuestions { get; set; }
+        public ApplicationUser LoggedInUser { get; set; }
+    }
+}
